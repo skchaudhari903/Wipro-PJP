d3fabb305b14e315520f345063a32280e7597362
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-03-18 00:54:58 +0530

    Push Arrays

---
+++ Java Fundamentals/src/Arrays/OneDimArray8.java
@@ -0,0 +1,38 @@
+/*Write a program to print the sum of the elements of an array following the given below condition.
+
+If the array has 6 and 7 in succeeding orders, ignore the numbers between 6 and 7 and consider the other numbers for calculation of sum.
+
+Eg1) Array Elements - 10,3,6,1,2,7,9
+O/P: 22   
+[i.e 10+3+9]
+
+Eg2) Array Elements - 7,1,2,3,6
+O/P:19
+
+Eg3) Array Elements - 1,6,4,7,9
+O/P:10
+*/
+
+package Arrays;
+
+public class OneDimArray8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array= {10, 3, 6, 1, 2, 7, 9};
+		int sum = 0;
+		int flag = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == 6)
+				flag = 1;
+			else if(array[i] == 7) {
+					flag = 0;
+					i++;
+			}
+			if(flag != 1)
+				sum = sum + array[i];
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ Java Fundamentals/src/Arrays/TwoDimArray.java
@@ -0,0 +1,55 @@
+/*Write a program to reverse the elements of a given 2*2 array. Four integer numbers needs to be passed as Command Line arguments.
+
+Example1)
+C:\>java Sample 1 2 3
+O/P: Please enter 4 integer numbers
+
+Example2)
+C:\>java Sample 1 2 3 4
+O/P: 
+ The given array is :
+  1 2 
+  3 4 
+ The reverse of the array is :
+  4 3 
+  2 1
+*/
+
+package Arrays;
+
+public class TwoDimArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length != 4) {
+			System.out.println("Please enter 4 integer numbers");
+		}
+		else {
+			int[][] array = new int[2][2];
+			int x = 0;
+			//storing elements
+			for (int i = 0; i < array.length; i++) {
+				for (int j = 0; j < array.length; j++) {
+					array[i][j] = Integer.parseInt(args[x++]);
+				}
+			}
+			
+			System.out.println("The given array is :");				
+			for (int i = 0; i < array.length; i++) {
+				for (int j = 0; j < array.length; j++) {
+					System.out.print(array[i][j] + "\t");
+				}
+				System.out.println();
+			}
+			
+			System.out.println("The reverse of the array is :");
+			for(int i = array.length-1; i >= 0; i--) {
+				for(int j = array.length-1; j >= 0; j--) {
+					System.out.print(array[i][j] + "\t");
+				}
+				System.out.println();
+			}
+		}
+	}
+
+}
