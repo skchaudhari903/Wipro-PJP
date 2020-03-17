711cb087e29f89cf577845b65d1361d6b0d7d411
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-03-17 23:50:29 +0530

    Push Arrays

--- Java Fundamentals/src/Arrays/OneDimArray5.java
+++ Java Fundamentals/src/Arrays/OneDimArray5.java
@@ -6,7 +6,18 @@ public class OneDimArray5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int[] array = {19, 14, 20, 18, 2};										//sort
+		for(int i = 0; i < array.length-1; i++) {
+			for(int j = 0; j < array.length-1; j++) {
+				if(array[j] > array[j+1]) {
+					int temp = array[j+1];
+					array[j+1] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+		System.out.println("Largest two numbers are "+array[array.length-1]+", "+array[array.length-2]);
+		System.out.println("Smallest two numbers are "+array[0]+", "+array[1]);
 	}
 
 }
