7de55d7a08cad6f2f0ac70e74271d6a5fe21c896
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-03-19 17:06:35 +0530

    Push Java Fundamentals

--- Java Fundamentals/src/Arrays/Assignment07.java
+++ Java Fundamentals/src/Arrays/Assignment07.java
@@ -15,18 +15,19 @@ public class Assignment07 {
 		int[] array = {12, 34, 12, 45, 67, 89};
 		
 		Arrays.sort(array);									//sort
-		int[] temp = new int[array.length];  
-        int j = 0;											//Using temporary array
-        for (int i = 0; i < array.length-1; i++){  
-            if (array[i] != array[i+1]){  
-                temp[j++] = array[i];  
-            }  
-         }  
-        temp[j++] = array[array.length-1];   
-        for (int i = 0; i < j; i++){  
-        	System.out.print(temp[i]+" ");  				//last element
-        }
+		int[] temp = new int[array.length];
+		int j = 0;											//Using temporary array
+		for (int i = 0; i < array.length-1; i++){
+			if (array[i] != array[i+1]){
+				temp[j++] = array[i];  
+			}
+		}
+		temp[j++] = array[array.length-1];
+		for (int i = 0; i < j; i++){
+			System.out.print(temp[i]+" ");  				//last element
+		}
 	}
+	
 }
 /*														
 		for(int i = 0; i < array.length; i++) {				//Without using temporary array
