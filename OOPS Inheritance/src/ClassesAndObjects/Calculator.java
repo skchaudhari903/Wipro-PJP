f74a91219e504a93b7300ff5a066da6cdfb62f3e
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-03-25 17:50:40 +0530

    Push Classes&Objects

---
+++ OOPS Inheritance/src/ClassesAndObjects/Calculator.java
@@ -0,0 +1,28 @@
+/*Create a new class called Calculator with the following methods: 
+1. A static method called powerInt(int num1,int num2)
+This method should return num1 to the power num2.
+2. A static method called powerDouble(double num1,int num2).
+This method should return num1 to the power num2.
+3. Invoke both the methods and test the functionalities.
+Hint: Use Math.pow(double,double) to calculate the power.
+*/
+
+package ClassesAndObjects;
+
+public class Calculator {
+	public static int powerInt(int num1, int num2) {
+		return (int) Math.pow(num1, num2);
+	}
+	
+	public static double powerDouble(double num1, int num2) {
+		return Math.pow(num1, num2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Calculator cal = new Calculator();
+		System.out.println(powerInt(12, 3));
+		System.out.println(powerDouble(1.5, 2));
+	}
+
+}
