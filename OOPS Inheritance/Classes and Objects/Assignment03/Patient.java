c75e0e112587187c3149033c67616d2170a0a7a2
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-03-26 21:17:01 +0530

    Push ClassesAndObjects

--- OOPS Inheritance/Classes and Objects/Assignment03/Patient.java
+++ OOPS Inheritance/Classes and Objects/Assignment03/Patient.java
@@ -17,12 +17,12 @@ public class Patient {
 	}
 	
 	double computeBMI() {
+		// BMI = ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
 		return ( weight / ( height * height ) ) * 703;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// BMI = ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
 		Patient patient = new Patient("XYZ", 177/2.54, 59*2.2);
 		System.out.println("Name: " + patient.patientName + "\nBMI: " + patient.computeBMI());
 	}
