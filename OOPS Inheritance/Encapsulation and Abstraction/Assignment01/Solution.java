a3cb1347835bbb898c02d7cae15a2cdcf29d2b16
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-03-28 22:52:30 +0530

    Push Encapsulation&Abstraction

---
+++ OOPS Inheritance/Encapsulation and Abstraction/Assignment01/Solution.java
@@ -0,0 +1,111 @@
+/*Create a class Author with the following information.
+Member variables : name (String), email (String), and gender (char)
+Parameterized Constructor: To initialize the variables
+
+
+Create a class Book with the following information.
+Member variables : name (String), author (of the class Author you have just created), price (double), and qtyInStock (int)
+[Assumption: Each book will be written by exactly one Author]
+Parameterized Constructor: To initialize the variables
+Getters and Setters for all the member variables
+
+In the main method, create a book object and print all details of the book (including the author details)
+*/
+
+package EncapsulationAbstraction;
+
+class Author {
+	private String name, email;
+	private char gender;
+	
+	Author(String name, String email, char gender){
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+	@Override
+	public String toString() {
+		return "Author [Name: " + name + ", Email: " + email + ", Gender: " + gender + "]";
+	}
+}
+
+class Book{
+	private String name;
+	private Author author;
+	private double price;
+	private int qtyInStock;
+	
+	Book(String name, Author author, double price, int qtyInStock){
+		this.name = name;
+		this.author = author;
+		this.price = price;
+		this.qtyInStock = qtyInStock;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQtyInStock() {
+		return qtyInStock;
+	}
+
+	public void setQtyInStock(int qtyInStock) {
+		this.qtyInStock = qtyInStock;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [Name: " + name + ", Author: " + author + ", Price: " + price + ", QtyInStock: " + qtyInStock + "]";
+	}
+}
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Author author = new Author("Shubham", "skc903@gmail.com", 'M');
+		Book book = new Book("Master in Java", author, 199.0, 500);
+		
+		System.out.println(book);
+	}
+
+}
+
