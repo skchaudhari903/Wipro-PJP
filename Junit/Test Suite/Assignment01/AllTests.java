0385f40740252a09104458b0cbe62853239deb25
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-04-19 12:15:09 +0530

    Push Test Suite

---
+++ Junit/Test Suite/Assignment01/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Demo1Test.class, Demo2Test.class, EmployeeTest.class })
+public class AllTests {
+
+}
