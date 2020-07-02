02240f0406bb476315639beb954462042849ef5f
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-07-02 21:43:57 +0530

    Push forEach

--- Java 8 Features/ForEach/Assignment01.java
+++ Java 8 Features/ForEach/Assignment01.java
@@ -1,11 +1,20 @@
+/*Write a program to create an ArrayList and add the weekdays. 
+Iterate and print all the elements using forEach method
+ */
+
+package forEach;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Assignmnet01{
-    public static void main(String[] args) {
-        ArrayList<String> arrL = new ArrayList<String>(
-                Arrays.asList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"));
+public class Assignment01 {
 
-        arrL.forEach(day -> System.out.println(day));
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> arrL = new ArrayList<String>(
+				Arrays.asList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"));
+		
+		arrL.forEach(day -> System.out.println(day));
+	}
+
 }
--- Java 8 Features/ForEach/Assignment02.java
+++ Java 8 Features/ForEach/Assignment02.java
@@ -1,3 +1,9 @@
+/* Create an ArrayList and add 5 Employee(id,name,address,salary) objects.
+Retrieve the objects from the ArrayList using forEach and print the Employee details.
+*/
+
+package forEach;
+
 import java.util.ArrayList;
 
 class Employee {
@@ -13,23 +19,25 @@ class Employee {
     }
 }
 
-public class Assignment02{
-    public static void main(String[] args) {
-        Employee emp1 = new Employee(1, 100.0, "James", "Kolkata");
-        Employee emp2 = new Employee(2, 200.0, "Kames", "Colkata");
-        Employee emp3 = new Employee(3, 300.0, "Lames", "Lolkata");
-        Employee emp4 = new Employee(4, 400.0, "Names", "Tolkata");
-        Employee emp5 = new Employee(5, 500.0, "Pames", "Bolkata");
+public class Assignment02 {
 
-        ArrayList<Employee> empList = new ArrayList<Employee>();
-        empList.add(emp1);
-        empList.add(emp2);
-        empList.add(emp3);
-        empList.add(emp4);
-        empList.add(emp5);
-
-        empList.forEach(emp -> System.out.println(
-                "ID : " + emp.id + " Name : " + emp.name + " Salary : " + emp.salary + " Address : " + emp.address));
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp1 = new Employee(1, 100.0, "James", "Kolkata");
+		Employee emp2 = new Employee(2, 200.0, "Kames", "Colkata");
+		Employee emp3 = new Employee(3, 300.0, "Lames", "Lolkata");
+		Employee emp4 = new Employee(4, 400.0, "Names", "Tolkata");
+		Employee emp5 = new Employee(5, 500.0, "Pames", "Bolkata");
+		
+		ArrayList<Employee> empList = new ArrayList<Employee>();
+		empList.add(emp1);
+		empList.add(emp2);
+		empList.add(emp3);
+		empList.add(emp4);
+		empList.add(emp5);
+		
+		empList.forEach(emp -> System.out.println(
+				"ID : " + emp.id + " Name : " + emp.name + " Salary : " + emp.salary + " Address : " + emp.address));
     }
 
 }
