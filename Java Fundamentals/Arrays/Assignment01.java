b43dc6e1513962ce7d02364761b6b78016da19f4
Abhishek Nallana <60889353+abhinallana@users.noreply.github.com>
2020-10-01 20:09:51 +0530

    updated comments in code

--- Java Fundamentals/Arrays/Assignment01.java
+++ Java Fundamentals/Arrays/Assignment01.java
@@ -6,16 +6,16 @@ public class Assignment01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] array = {19, 14, 20, 18, 2};
-		int sum = 0;
+		int[] array = {19, 14, 20, 18, 2};	//initialised an array..
+		int sum = 0;	//Assuming sum count from 0
 		
-		System.out.println("Array: "+Arrays.toString(array)); 						//print Array
+		System.out.println("Array: "+Arrays.toString(array)); 		//print Array
 		
-		for(int i = 0; i < array.length; i++) {										//sum
+		for(int i = 0; i < array.length; i++) {				//summing up all the elements of the array
 			sum = sum + array[i];
 		}
 		System.out.println("The sum of the array is: "+sum);
-		double avg = (sum/array.length);											//average
+		double avg = (sum/array.length);						//average of the array
 		System.out.println("The average of the array is: "+avg);	
 	}
 
