bd51115f5467255cdb7572c157993794290c0bbd
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-10-09 11:24:00 +0530

    Revert "Hibernate, Spring and Spring boot"

--- Spring-Boot/SpringBoot-MVC-JPA-H2/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.abhishek.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.abhishek.model.Employee;
-public interface EmployeeRepository extends CrudRepository<Employee, Integer>
-{
-}
